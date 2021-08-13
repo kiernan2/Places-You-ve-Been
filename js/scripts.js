e8e4b5d58b991bd124f6616f994fa5f974026315
Kiernan2 <kiernan1994@gmail.com>
2021-08-13 15:55:38 -0700

    It now gathers inputs from the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,46 @@
 // Business Logic
-function place(location,landmarks,time,notes) {
+function Places() {
+  this.places = {};
+  this.currentId = 0;
+}
+
+function Place(location,landmarks,time,notes) {
   this.location = location;
   this.landmarks = landmarks;
   this.time = time;
   this.notes = notes;
 }
 
-let place1 = new place("Istanbul", "b", "c", "o")
+Places.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+Places.prototype.addPlace = function(place) {
+  place.id = this.assignId();
+  this.places[place.id] = place;
+}
+
+let place1 = new Place("Istanbul", "b", "c", "o")
 
 // UI Logic
 $(document).ready(function() {
+  let places = new Places();
   $("#place1").text(place1.location);
   $("#stuff1").text(place1.landmarks + " " + place1.time + " " + place1.notes);
   $("#place1").click(function(){
-    $("#stuff1").show()
+    console.log("hi")
+    $("#stuff1").toggle();
+  });
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    let location = ($("input#inputLocation").val());
+    let landmarks = ($("input#inputLandmarks").val());
+    let time = ($("input#inputTime").val());
+    let notes = ($("input#inputNotes").val());
+    let place = new Place(location,landmarks,time,notes);
+    places.addPlace(place);
+    console.log(places);
   });
 });
 
